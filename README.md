626a25ed2415ae0b7a8cf0d6b992b67ba24c11e3
Ayushkumar Maurya <ayushskmaurya@gmail.com>
2021-03-12 14:47:27 +0530

    Top 10 similar movies predicted

--- README.md
+++ README.md
@@ -5,10 +5,16 @@ This website will recommend movies to the user based on genres, actors and relea
 Movie datasets are taken from Kaggle and is present in `datasets` folder.<br />
 `prepare_data.ipynb` is used to prepare data and the prepared data is present in `prepared_data` folder.<br />
 
+Run `predict_similar_movies.py` to get top 10 similar movies.
+
 ### Instructions:
 1. Install Jupyter Notebook using the following command:
 	```
 	pip install notebook
+	```
+	To run the notebook:
+	```
+	jupyter notebook
 	```
 
 2. Install pandas package using the following command:
---
+++ predict_similar_movies.py
@@ -0,0 +1,65 @@
+import pandas as pd
+
+# Reading prepared dataset.
+movie_pre_info = pd.read_csv('prepared_data/movie_pre_info.csv')
+
+year_diff = 2019 - 1950
+movie_pre_info['year_of_release'] = pd.to_numeric(movie_pre_info['year_of_release'], errors='coerce')
+
+
+# Returning genres, actors and released date of the movie.
+def get_info(title):
+    row = movie_pre_info[movie_pre_info['title'].str.lower() == title.lower()]
+    data = next(row.iterrows(), None)
+    
+    if data is not None:
+        genres = data[1].genres.split("|")
+        actors = data[1].actors.split("|")[:-1]
+        return {
+            "genres": genres,
+            "actors": actors,
+            "year_of_release": int(data[1].year_of_release),
+            "genre_count": len(genres),
+            "actor_count": len(actors)
+        }
+    
+    else:
+        return None
+
+
+# Calculating the probability of similarity between the movies.
+def probability(row, info):    
+    counts = {'genre': 0, 'actor': 0}
+    
+    for genre in info['genres']:
+        if genre in row[1]:
+            counts['genre'] += 1
+    
+    for actor in info['actors']:
+        if actor in row[2]:
+            counts['actor'] += 1
+    
+    counts['diff_year'] = abs(info['year_of_release'] -  row[3])
+        
+    p = 0.5 * (counts['genre'] / info['genre_count'])
+    p += 0.3 * (counts['actor'] / info['actor_count'])
+    p += 0.2 * (1 - (counts['diff_year'] / year_diff))
+    return  p
+
+
+# Getting the list of top 10 similar movies.
+def get_similar_movies(title):
+    info = get_info(title)
+    if info is not None:
+        movie_pre_info['p'] = movie_pre_info.apply(lambda row: probability(row, info), axis=1)
+        pre_movies = movie_pre_info[movie_pre_info['title'].str.lower() != title.lower()]
+        return pre_movies.nlargest(10, 'p')
+    else:
+        return "No movie found!"
+
+
+title = input("Enter movie title: ")
+similar_movies = get_similar_movies(title)['title'].tolist()
+print("\nTop 10 similar movies:")
+for movie in similar_movies:
+	print(movie)
