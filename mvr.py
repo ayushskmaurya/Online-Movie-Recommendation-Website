4aa5a3bbc02aa14e2e3777b63262dbec9b505fa4
Ayushkumar Maurya <ayushskmaurya@gmail.com>
2021-03-19 23:58:08 +0530

    Created dropdown for top 5 rated movies while searching

--- mvr.py
+++ mvr.py
@@ -1,5 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import pandas as pd
 app = Flask(__name__)
+
+
+# Show top 5 rated movies while searching
+@app.route("/show_movies", methods=['POST'])
+def show_movies():
+	text = request.form['text']
+
+	movie_pre_info = pd.read_csv('prepared_data/movie_details.csv')
+	movies = movie_pre_info[movie_pre_info['title'].str.match("(?i)" + text)]
+	movies = movies.nlargest(5, 'imdb_rating')
+	
+	return jsonify(movies['title'].tolist())
 
 
 @app.route("/")
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,21 @@
+// Show top 5 rated movies while searching
 function show_movies(text) {
-	if(text.length >= 3)
+	if(text.length >= 3) {
+		$.ajax({
+			url: "/show_movies",
+			method: "POST",
+			data: {text:text},
+			success: function(movies) {
+				document.getElementById("movies").innerHTML = "";
+				for(let i in movies) {
+					let div = document.createElement("DIV");
+					div.innerHTML = "<button class='movie'>" + movies[i] + "</button>";
+					document.getElementById("movies").appendChild(div);
+				}
+			}
+		});
 		document.getElementById("movies").style.display = "block";
+	}
 	else
 		document.getElementById("movies").style.display = "none";
 }
