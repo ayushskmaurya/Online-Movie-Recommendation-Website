9eadc97d13c9ec5402f5fbc329e582d74f9b4e89
Ayushkumar Maurya <ayushskmaurya@gmail.com>
2021-03-20 17:19:32 +0530

    Displaying searched movie details

--- mvr.py
+++ mvr.py
@@ -15,6 +15,35 @@ def show_movies():
 	return jsonify(movies['title'].tolist())
 
 
+# Returning details of particular movie
+def get_info(movie_pre_info, title):
+    row = movie_pre_info[movie_pre_info['title'].str.lower() == title.lower()]
+    data = next(row.iterrows(), None)
+    
+    if data is not None:
+        return {
+            "title": data[1].title,
+            "genres": data[1].genres,
+            "actors": data[1].actors,
+            "release_date": data[1].release_date,
+            "imdb_rating": data[1].imdb_rating,
+            "summary": data[1].summary,
+            "poster_path": data[1].poster_path,
+            "wiki_link": data[1].wiki_link
+        }
+    
+    else:
+        return "0"
+
+
+# Retrieving movie details from prepared csv file
+@app.route("/movie_details", methods=['POST'])
+def movie_details():
+	title = request.form['title']
+	movie_pre_info = pd.read_csv('prepared_data/movie_details.csv')
+	return get_info(movie_pre_info, title)
+
+
 @app.route("/")
 def index():
 	return render_template("index.html")
--- static/js/main.js
+++ static/js/main.js
@@ -9,7 +9,7 @@ function show_movies(text) {
 				document.getElementById("movies").innerHTML = "";
 				for(let i in movies) {
 					let div = document.createElement("DIV");
-					div.innerHTML = "<button class='movie'>" + movies[i] + "</button>";
+					div.innerHTML = "<button type='button' class='movie' onclick='search(this.value);' value='" + movies[i] + "'>" + movies[i] + "</button>";
 					document.getElementById("movies").appendChild(div);
 				}
 			}
@@ -18,4 +18,34 @@ function show_movies(text) {
 	}
 	else
 		document.getElementById("movies").style.display = "none";
+}
+
+// Getting movie name and searching for movie details
+function movie_name() {
+	let title = document.getElementById("title").value.trim();
+	search(title);
+}
+
+// Retrieving movie details from prepared csv file
+function search(title) {
+	$.ajax({
+		url: "/movie_details",
+		method: "POST",
+		data: {title:title},
+		success: function(data) {
+			if(data != "0") {
+				document.getElementById("title").value = "";
+				document.getElementById("movies").style.display = "none";
+				document.getElementById("movie-poster").src = data['poster_path'];
+				document.getElementById("movie-poster").alt = data['title'];
+				document.getElementById("movie-title").innerHTML = data['title'];
+				document.getElementById("movie-genres").innerHTML = data['genres'];
+				document.getElementById("movie-imdb-rating").innerHTML = data['imdb_rating'];
+				document.getElementById("movie-release-date").innerHTML = data['release_date'];
+				document.getElementById("movie-cast").innerHTML = data['actors'];
+				document.getElementById("movie-summary").innerHTML = data['summary'];
+				document.getElementById("wiki").href = data['wiki_link'];
+			}
+		}
+	});
 }
